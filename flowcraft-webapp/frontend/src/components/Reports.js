e241f71754357979744700bc881f9ed678aa8996
odiogosilva <o.diogosilva@gmail.com>
2018-08-09 09:19:58 +0100

    Fixed component order

--- flowcraft-webapp/frontend/src/components/Reports.js
+++ flowcraft-webapp/frontend/src/components/Reports.js
@@ -29,73 +29,6 @@ import {TaskButtons} from "./reports/task_buttons"
 
 
 /**
- * Entry point for /reports URL
- *
- * Full component for Reports home page. It is responsible for handling
- * the Drag and Drop of report files OR the specification of runID for
- * fetching report data to the database. Other Home components can be
- * added depending on the service option stored in
- * flowcraft/frontend/config.json
- *
- * These components are responsible for gathering the report data JSON array
- * and then redirect to the /reports/app URL providing the report data in
- * the state of the URL.
- *
- */
-export class ReportsHome extends DraggableView{
-
-    constructor(props){
-        super(props);
-
-        this.props.history.push("/reports");
-
-        this.state = {
-            "runId": "",
-            "reportData": null,
-            "openModal": false,
-            "dropData": []
-        };
-    }
-
-    render() {
-
-        return(
-            <div>
-                {
-                    this.state.reportData &&
-                    <DragAndDropModal openModal={this.state.openModal}
-                                      setModalState={this.setModalState}
-                                      dropData={this.state.dropData}
-                                      mergeReports={this.mergeReports}
-                                      loadReports={this.loadReports}/>
-                }
-                {
-                    this.state.reportData ?
-                        <Redirect to={{
-                            pathname: "/reports/app",
-                            state: {"data": this.state.reportData}
-                        }}/> :
-                        <div>
-                            {
-                                service === "innuendo" ?
-                                    <div>
-                                        <Header headerTitle={"INNUENDO Reports"}/>
-                                        <HomeInnuendo route={"reports"}/>
-                                    </div>:
-                                    <div>
-                                        <Header headerTitle={"Reports"}/>
-                                        <HomeInput route={"reports"}/>
-                                    </div>
-                            }
-                        </div>
-                }
-            </div>
-        )
-    }
-}
-
-
-/**
  * This is a base component that provides drag and drop functionality to the
  * reports home and reports app routes. It should be used as an extension
  * for these components only, not for general use.
@@ -236,6 +169,72 @@ class DragAndDropModal extends React.Component {
     }
 }
 
+
+/**
+ * Entry point for /reports URL
+ *
+ * Full component for Reports home page. It is responsible for handling
+ * the Drag and Drop of report files OR the specification of runID for
+ * fetching report data to the database. Other Home components can be
+ * added depending on the service option stored in
+ * flowcraft/frontend/config.json
+ *
+ * These components are responsible for gathering the report data JSON array
+ * and then redirect to the /reports/app URL providing the report data in
+ * the state of the URL.
+ *
+ */
+export class ReportsHome extends DraggableView{
+
+    constructor(props){
+        super(props);
+
+        this.props.history.push("/reports");
+
+        this.state = {
+            "runId": "",
+            "reportData": null,
+            "openModal": false,
+            "dropData": []
+        };
+    }
+
+    render() {
+
+        return(
+            <div>
+                {
+                    this.state.reportData &&
+                    <DragAndDropModal openModal={this.state.openModal}
+                                      setModalState={this.setModalState}
+                                      dropData={this.state.dropData}
+                                      mergeReports={this.mergeReports}
+                                      loadReports={this.loadReports}/>
+                }
+                {
+                    this.state.reportData ?
+                        <Redirect to={{
+                            pathname: "/reports/app",
+                            state: {"data": this.state.reportData}
+                        }}/> :
+                        <div>
+                            {
+                                service === "innuendo" ?
+                                    <div>
+                                        <Header headerTitle={"INNUENDO Reports"}/>
+                                        <HomeInnuendo route={"reports"}/>
+                                    </div>:
+                                    <div>
+                                        <Header headerTitle={"Reports"}/>
+                                        <HomeInput route={"reports"}/>
+                                    </div>
+                            }
+                        </div>
+                }
+            </div>
+        )
+    }
+}
 
 /**
  * This is the main component interface with the reports app. The reports
