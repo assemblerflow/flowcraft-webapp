0e02cbf0dc30390d7852fce40590b96a45865f56
odiogosilva <o.diogosilva@gmail.com>
2018-09-12 11:11:21 +0100

    Finished home page

--- flowcraft-webapp/frontend/src/components/Home.js
+++ flowcraft-webapp/frontend/src/components/Home.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 import Typography from "@material-ui/core/Typography"
 import Button from "@material-ui/core/Button";
+import Fade from '@material-ui/core/Fade';
 import Grid from "@material-ui/core/Grid";
 
 import logo from  "../../resources/Logo_small.png";
@@ -12,6 +13,11 @@ import {themes} from "./reports/themes";
 import {theme} from "../../config.json";
 
 class Home extends React.Component {
+
+    state = {
+        showInspectTooltip: false,
+        showReportsTooltip: false
+    };
 
     render () {
 
@@ -54,6 +60,15 @@ class Home extends React.Component {
             notice: {
                 marginTop: "10px",
                 textAlign: "center",
+                fontSize: "1.3vmax"
+            },
+            tooltipContainer: {
+                position: "relative"
+            },
+            tooltip: {
+                position: "absolute",
+                top: 0,
+                fontSize: "1.4vmax"
             }
         };
 
@@ -62,16 +77,34 @@ class Home extends React.Component {
                 <div style={style.logoContainer}>
                     <img style={style.logo} src={logo} alt={"logo"}/>
                 </div>
-                <Typography style={style.headerText}>A Nextflow workflow assembler for genomics</Typography>
+                <Typography style={style.headerText}>A Nextflow pipeline assembler for genomics</Typography>
                 <Typography style={style.notice}>We are currently building the website but the following services are already available:</Typography>
                 <Grid container spacing={24} style={style.buttonContainer}>
                     <Link to={"inspect"} style={{textDecoration: "none"}}>
-                        <Button variant={"outlined"} color={"primary"} style={style.mainButton}>Inspect</Button>
+                        <Button
+                            onMouseEnter={() => {this.setState({showInspectTooltip: true})}}
+                            onMouseLeave={() => {this.setState({showInspectTooltip: false})}}
+                            variant={"outlined"}
+                            color={"primary"}
+                            style={style.mainButton}>Inspect</Button>
                     </Link>
                     <Link to={"reports"} style={{textDecoration: "none"}}>
-                        <Button variant={"outlined"} color={"primary"} style={style.mainButton}>Reports</Button>
+                        <Button
+                            onMouseEnter={() => {this.setState({showReportTooltip: true})}}
+                            onMouseLeave={() => {this.setState({showReportTooltip: false})}}
+                            variant={"outlined"}
+                            color={"primary"}
+                            style={style.mainButton}>Reports</Button>
                     </Link>
                 </Grid>
+                <div style={style.tooltipContainer}>
+                    <Fade in={this.state.showInspectTooltip}>
+                        <Typography style={style.tooltip} align={"center"}>Monitor the execution of a running Nextflow/Flowcraft pipeline</Typography>
+                    </Fade>
+                    <Fade in={this.state.showReportTooltip}>
+                        <Typography style={style.tooltip} align={"center"}>See the interactive reports from a Flowcraft pipeline execution</Typography>
+                    </Fade>
+                </div>
             </div>
         )
     }
